b2ecd989be7d2f0bb664a65b14db4248a12ccac7
khaled93shadid <khaled_shadid@yahoo.com>
2025-12-23 23:39:56 +0300

    finsh main product page

---
+++ main.js
@@ -0,0 +1,37 @@
+console.log("Java Script SwitchedOn You Are In Good Hand")
+import { products } from "./data/data.js";
+
+
+
+// all products container in main page start
+const render_products = () => {
+
+  let productsHTML = '';
+  products.forEach((product) => {
+    const html = `  
+     
+      <div class="product-card card  p-4 shadow-lg border-2">
+       <img class="product-card-img " src=${product.image} alt="">
+       <p class="fw-bold text-center mt-2 product-name">${product.name}</p>
+       <div class="star-container d-flex align-items-center justify-content-center">
+         <img src="./images/ratings/rating-${(product.rating.stars)*10}.png"  class="w-50" alt="">
+         <p class="lead fw-light mt-3 ms-2">${product.rating.count}</p>
+       </div>
+        <p class="text-center fw-bold ">${((product.priceCents)/100).toFixed(2)}<span class="ps-1">$</span></p>
+        <button class="btn btn-outline-dark border-2 fw-bold">Add To Cart</button>
+    </div>
+     
+     `
+    // generate one product card each iteration
+    productsHTML+=html;
+
+  })//end for each add all cards products
+
+  document.querySelector('.all-products-container').innerHTML=productsHTML
+  console.log('display all products with javaScripts successfuly')
+
+}
+
+render_products()
+// all products container in main page end
+
